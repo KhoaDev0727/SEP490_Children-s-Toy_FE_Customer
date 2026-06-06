20fb2bfa0537170e6bfce1da616fdbdf04d49ee5
KhoaDev0727 <dannykhoa88@gmail.com>
2026-06-06 17:42:55 +0700

    fix homepage for deploy

--- src/app/(customer)/profile/vouchers/page.tsx
+++ src/app/(customer)/profile/vouchers/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Metadata } from "next";
 import ProfileSidebar from "../_components/ProfileSidebar";
 import VoucherList from "@/features/vouchers/components/VoucherList";
@@ -11,7 +12,9 @@ export default function VouchersPage() {
   return (
     <div className="grow max-w-[1280px] mx-auto w-full px-4 md:px-8 py-12 grid grid-cols-1 md:grid-cols-4 gap-6">
       <ProfileSidebar />
-      <VoucherList />
+      <Suspense fallback={<div className="md:col-span-3">Loading...</div>}>
+        <VoucherList />
+      </Suspense>
     </div>
   );
 }
